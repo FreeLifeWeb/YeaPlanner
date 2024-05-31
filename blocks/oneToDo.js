f9b2ffb599f60bd5ad38d796664fafa8dbed3fee
Anton <antosha.uvarov.90@mail.ru>
2024-06-01 01:00:39 +0300

    second commit: optimization

---
+++ blocks/oneToDo.js
@@ -0,0 +1,19 @@
+export const oneToDo = ` <div class="element-of-list">
+<div class="checkbox-container">
+    <input
+        type="checkbox"
+        id="round-checkbox"
+        class="round-checkbox"
+    />
+    <label
+        for="round-checkbox"
+        class="round-checkbox-label"
+    ></label>
+</div>
+<div>
+    <p class="task">My first task</p>
+</div>
+<div>
+    <img src="./img/trash.svg" alt="trash" />
+</div>
+</div>`;
---
+++ blocks/withoutTodo.js
@@ -0,0 +1,14 @@
+export const withoutToDo = `<div id="todo-list">
+<div>
+    <img
+        src="./img/Clipboard.svg"
+        alt="Clipboard"
+        class="clipboard"
+    />
+</div>
+<div>
+    <p class="no-things-text">
+        There are no things at the moment
+    </p>
+</div>
+</div>`;
